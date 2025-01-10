5381b1cde2e18c4fb7025343aa934286c8f8fb38
cronocl <alwin.gobimbel@gmail.com>
2025-01-10 17:01:20 +0700

    enhance structure

--- alwin-go/internal/domain/Role.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-// Role represents a user role in the system
-type Role struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
--- alwin-go/internal/domain/User.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-// User represents a user entity
-type User struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"` // example additional field
-}
